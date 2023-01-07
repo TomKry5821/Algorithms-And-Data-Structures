f336b3720b487e08a818c7e3c010871f4f8032a9
TomKry5821 <tomakry729@student.polsl.pl>
2023-01-08 00:53:28 +0100

    Added addToStart and addToEnd methods

--- src/main/java/pl/tk/datastructures/SingleLinkedList.java
+++ src/main/java/pl/tk/datastructures/SingleLinkedList.java
@@ -7,6 +7,28 @@ import java.util.Objects;
 public class SingleLinkedList {
     SingleLinkedListNode head = null;
 
+    public void addToEnd(int data) {
+        if (Objects.isNull(this.head)) {
+            head = new SingleLinkedListNode(data);
+            return;
+        }
+        var headCopy = head;
+        while (!Objects.isNull(headCopy.next)) {
+            headCopy = headCopy.next;
+        }
+        headCopy.next = new SingleLinkedListNode(data);
+    }
+
+    public void addToStart(int data) {
+        if (Objects.isNull(this.head)) {
+            head = new SingleLinkedListNode(data);
+            return;
+        }
+        var newNode = new SingleLinkedListNode(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
     public Integer[] toArray() {
         List<Integer> arr = new ArrayList<>();
         var headCopy = head;
