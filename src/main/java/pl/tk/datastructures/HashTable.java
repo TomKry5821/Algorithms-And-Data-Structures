dd8fa6ebc3b2ad9272703ccd78240bed537e6a2e
TomKry5821 <tomakry729@student.polsl.pl>
2023-01-11 23:05:23 +0100

    Added add, get and remove methods

--- src/main/java/pl/tk/datastructures/HashTable.java
+++ src/main/java/pl/tk/datastructures/HashTable.java
@@ -11,7 +11,7 @@ public class HashTable {
         int hashCode;
         HashNode next;
 
-        public HashNode(int key, int value, int hashCode){
+        public HashNode(int key, int value, int hashCode) {
             this.key = key;
             this.value = value;
             this.hashCode = hashCode;
@@ -23,12 +23,12 @@ public class HashTable {
     int bucketsNumber;
     int size;
 
-    public HashTable(){
+    public HashTable() {
         buckets = new ArrayList<>();
         bucketsNumber = 10;
         size = 0;
 
-        for(int i = 0; i < bucketsNumber; i++){
+        for (int i = 0; i < bucketsNumber; i++) {
             buckets.add(null);
         }
     }
@@ -41,9 +41,90 @@ public class HashTable {
         return size;
     }
 
-    private int hashCode(int data){
+    private int hashCode(int data) {
         return Objects.hashCode(data);
     }
 
+    private int getBucketIndex(int key) {
+        var keyHash = this.hashCode(key);
+        var index = keyHash % bucketsNumber;
+        return Math.abs(index);
+    }
 
+    public int remove(int key) {
+        var bucketIndex = getBucketIndex(key);
+        var keyHash = hashCode(key);
+        var hashNode = buckets.get(bucketIndex);
+
+        if(Objects.isNull(hashNode.next)){
+            buckets.set(bucketIndex, null);
+            size -= 1;
+        }
+
+        while (Objects.nonNull(hashNode.next)) {
+            if (hashNode.next.key == key && keyHash == hashNode.next.hashCode) {
+                hashNode.next = hashNode.next.next;
+                size -= 1;
+                return hashNode.value;
+            }
+        }
+        return -1;
+    }
+
+    public int add(int key, int value) {
+        var bucketIndex = getBucketIndex(key);
+        var keyHash = hashCode(key);
+        var hashNode = buckets.get(bucketIndex);
+
+        var newHashNode = new HashNode(key, value, keyHash);
+        if (Objects.isNull(hashNode)) {
+            hashNode = newHashNode;
+            buckets.set(bucketIndex, hashNode);
+            size += 1;
+            return value;
+        }
+
+        while (Objects.nonNull(hashNode.next)) {
+            if (hashNode.key == key && hashNode.hashCode == keyHash) {
+                hashNode.value = value;
+                return value;
+            }
+            hashNode = hashNode.next;
+        }
+
+        newHashNode.next = buckets.get(bucketIndex);
+        buckets.set(bucketIndex, newHashNode);
+
+        this.size += 1;
+
+        if ((1.0 * size) / bucketsNumber >= 0.7) {
+            ArrayList<HashNode> temp = buckets;
+            buckets = new ArrayList<>();
+            bucketsNumber = 2 * bucketsNumber;
+            size = 0;
+            for (int i = 0; i < bucketsNumber; i++)
+                buckets.add(null);
+
+            for (HashNode headNode : temp) {
+                while (headNode != null) {
+                    add(headNode.key, headNode.value);
+                    headNode = headNode.next;
+                }
+            }
+        }
+        return value;
+    }
+
+    public int get(int key) {
+        var bucketIndex = getBucketIndex(key);
+        var keyHash = hashCode(key);
+        var hashNode = buckets.get(bucketIndex);
+
+        while (Objects.nonNull(hashNode)) {
+            if (hashNode.key == key && keyHash == hashNode.hashCode) {
+                return hashNode.value;
+            }
+        }
+        return -1;
+    }
 }
