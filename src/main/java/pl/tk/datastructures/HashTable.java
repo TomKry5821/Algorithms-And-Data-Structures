1a09895e7998d1b6ff77413726c02d41e97e126f
TomKry5821 <tomakry729@student.polsl.pl>
2023-01-11 21:16:01 +0100

    Added HashNode class and constructors

--- src/main/java/pl/tk/datastructures/HashTable.java
+++ src/main/java/pl/tk/datastructures/HashTable.java
@@ -1,4 +1,34 @@
 package pl.tk.datastructures;
 
+import java.util.ArrayList;
+
 public class HashTable {
+
+    class HashNode {
+        int key;
+        int value;
+        int hashCode;
+        HashNode next;
+
+        public HashNode(int key, int value, int hashCode){
+            this.key = key;
+            this.value = value;
+            this.hashCode = hashCode;
+            this.next = null;
+        }
+    }
+
+    ArrayList<HashNode> buckets;
+    int bucketsNumber;
+    int size;
+
+    public HashTable(){
+        buckets = new ArrayList<>();
+        bucketsNumber = 10;
+        size = 0;
+
+        for(int i = 0; i < bucketsNumber; i++){
+            buckets.add(null);
+        }
+    }
 }
